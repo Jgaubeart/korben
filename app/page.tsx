bc89ed4b897347d63b4521f3f51221a79dc582da
Jgaubeart <jgaubeart@gmail.com>
2026-09-19 09:51:35 -0400

    Fix Supabase client import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import { createBrowserSupabaseClient } from "@/lib/supabase/client";
+import { createBrowserSupabaseClient } from "../lib/supabase/client";
 
 type Message = {
   id?: string;
